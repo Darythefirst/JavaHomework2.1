6428527a34a667607d9bd48f02d9c928acb2c312
Dary <Dafna_pelmeshka@mail.ru>
2023-04-03 22:42:36 +0300

    Main changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int ticketPrice = 27891; //стоимость билета
+        int bonusMile = 20; //рублей для одной бонусной мили
+        int totalBonusMile = ticketPrice / bonusMile; //итоговая сумма бонусных миль
+
+        System.out.println(totalBonusMile + " бонусных миль");
+
 
     }
 }
